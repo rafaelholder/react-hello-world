40a3cf34052221fd8770ae643acef18c87f6e21d
Rafael <rafaelholder0106@gmail.com>
2023-01-26 20:38:35 -0300

    correção import Prop-types

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,7 @@
 import './styles.css';
 
+import P from 'prop-types'
+
 export const Button = ({ text, onClick, disabled }) => (
   <button className='button' onClick={onClick} disabled={disabled}>
     {text}
